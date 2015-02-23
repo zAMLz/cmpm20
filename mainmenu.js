c7e9a09b5cc44a2b8cf9c28186b49b8b1d42be6d
ttagawa <tylertagawa@gmail.com>
2015-02-22 17:24:00 -0800

    added music to the mainmenu and game

--- mainmenu.js
+++ mainmenu.js
@@ -2,20 +2,27 @@ var Game = {}
 var menuText;
 Game.mainmenu = function (game){
 	this.playButton = null;
+	this.music=null
 };
 
 Game.mainmenu.prototype = {
 	preload: function(){	
     this.load.image('sky', 'assets/sky.png');
     this.load.image('play', 'assets/playButton.png');
+    //also needs ogg version for firefox.
+    this.load.audio('menumusic', 'assets/Steve_Combs_13_Our_Wasted_Youth.mp3');
 	},
 
 	create: function(){
+		//adds music
+		this.music = this.add.audio('menumusic');
+		this.music.play();
 		this.add.sprite(0,0,'sky');
 		menuText = this.add.text(380, 150, 'Main Menu', { fontSize: '32px', fill: '#fff' });
 		this.playButton=this.add.button(400,300, 'play',this.startGame,this);
 	},
 	startGame: function(pointer){
+		this.music.stop();
 		this.state.start('main');
 	}
 }
