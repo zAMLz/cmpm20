512b69f8bd23f801e87a7cc26b83770f6adbd877
ttagawa <tylertagawa@gmail.com>
2015-02-27 18:06:14 -0800

    preloader works with tweening. Just needs mainmenu start button

--- mainmenu.js
+++ mainmenu.js
@@ -1,4 +1,3 @@
-var Game = {}
 var menuText;
 Game.mainmenu = function (game){
 	this.playButton = null;
@@ -6,20 +5,16 @@ Game.mainmenu = function (game){
 };
 
 Game.mainmenu.prototype = {
-	preload: function(){	
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('play', 'assets/playButton.png');
-    //also needs ogg version for firefox.
-    this.load.audio('menumusic', 'assets/Steve_Combs_13_Our_Wasted_Youth.mp3');
-	},
-
+	
 	create: function(){
 		//adds music
 		this.music = this.add.audio('menumusic');
 		this.music.play();
 		this.add.sprite(0,0,'sky');
-		menuText = this.add.text(380, 150, 'Main Menu', { fontSize: '32px', fill: '#fff' });
-		this.playButton=this.add.button(400,300, 'play',this.startGame,this);
+		menuText = this.add.text(300, 400, 'Main Menu', { fontSize: '32px', fill: '#fff' });
+		menuText.anchor.setTo(0.5,0.5);
+		this.playButton=this.add.button((this.world.width-100)/2, (this.world.height-100)/2, 'play',this.startGame,this);
+		//this.playButton.anchor.setTo(0.5, 0.5);
 	},
 	startGame: function(pointer){
 		this.music.stop();
