65b5e004030d6fc2cec12a16b53fd5cc68917bb8
ttagawa <tylertagawa@gmail.com>
2015-03-05 16:31:29 -0800

    moved asset loading to preloader and added a ladder

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ var counter = 0;
 var checkmark;
 var index;
 var star;
+var ladder;
 
 //-------------Boxes------------------
 var checkCreated = 0;
@@ -53,47 +54,6 @@ Game.main = function(game){
 }
 Game.main.prototype={
     
-
-    preload: function () {
-        this.load.audio('tutorialmusic', 'assets/audio/Steve_Combs_22_Thank_You_Remix.mp3');
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-        this.load.image('fulldome', 'assets/fulldome.png');
-        this.load.image('diamond','assets/diamond.png');
-        this.load.image('check','assets/check.png');
-        //Terrain details
-        this.load.physics('physicsdata','assets/world/forest/forest.json');
-        this.load.image('terr-null','assets/world/forest/terr-null.png');
-        this.load.image('terr1-1','assets/world/forest/terr1-1.png');
-        this.load.image('terr1-2','assets/world/forest/terr1-2.png');
-        this.load.image('terr1-3','assets/world/forest/terr1-3.png');
-        this.load.image('terr1-4','assets/world/forest/terr1-4.png');
-        this.load.image('terr1-5','assets/world/forest/terr1-5.png');
-        this.load.image('terr1-6','assets/world/forest/terr1-6.png');
-        this.load.image('terr1-7','assets/world/forest/terr1-7.png');
-        this.load.image('terr1-b','assets/world/forest/terr1-b.png');
-        this.load.image('terr1-b2','assets/world/forest/terr1-b2.png');
-        this.load.image('water1-1','assets/world/forest/water1-1.png');
-        //UI
-        this.load.image('continue','assets/UI/continue.png');
-        this.load.image('help','assets/UI/help.png');
-        this.load.image('pause','assets/UI/pause.png');
-        this.load.image('quit','assets/UI/quit.png');
-        this.load.image('restart','assets/UI/restart.png');
-        this.load.image('helpscn','assets/UI/helpscreen.png');
-
-        //fire
-        this.load.image('fire1', 'assets/fire/fire1.png');
-        this.load.image('fire2', 'assets/fire/fire2.png');
-        this.load.image('fire3', 'assets/fire/fire3.png');
-        this.load.image('smoke', 'assets/fire/smoke-puff.png');
-
-        this.load.spritesheet('ball', 'assets/fire/plasmaball.png', 128, 128);
-
-    },
-
     createBox: function(x, y, index, playerCollisionGroup, isJumpCollisionGroup ){
         boxX = x;
         boxY = y;
@@ -190,6 +150,8 @@ Game.main.prototype={
         this.physics.p2.enableBody(star, isDebug);
         star.body.setCollisionGroup(winCollisionGroup);
         star.body.collides([isJumpCollisionGroup, playerCollisionGroup]);
+        //climbable tree
+        ladder = this.add.sprite(200,1560,'ladder');
         
         // The player aanimations and position
         player = this.add.sprite(32, 1600 - 150, 'dude');
