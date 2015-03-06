82d18f2f961ee0d881fef1b9958a618fdd7885c9
ttagawa <tylertagawa@gmail.com>
2015-03-06 00:19:59 -0800

    added traps and a ladder to get around the traps

--- tutorial.js
+++ tutorial.js
@@ -1,21 +1,61 @@
-// Phaser this File
-//var this = new Phaser.this(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player;
 var platforms;
 var cursors;
 var facing = 'left';
 var jumpButton;
+var pushButton;
 var isDebug = true;
 var ifCanJump = true;
-var trap;
-
 var star;
+var ladder;
+var pausePanel;
+var paused;
+
+//collision groups
+var playerCollisionGroup;
+var isJumpCollisionGroup;
+var winCollisionGroup;
+var killCollisionGroup;
+var BoxCollisionGroup;
+
+//-------------Boxes------------------
+var checkCreated = 0;
+var Box;
+var boxX;
+var boxY;
+var onGround = true;
+var playerbox = true;
+
+
 var score = 0;
 var scoreText;
 Game.tutorial = function(game){
     this.music=null;
 }
 Game.tutorial.prototype={
+    createBox: function(x, y, index, playerCollisionGroup, isJumpCollisionGroup,BoxCollisionGroup ){
+        boxX = x;
+        boxY = y;
+        Box = this.add.sprite(x, y, index);
+        this.physics.p2.enableBody(Box);
+        Box.body.friction = 100;
+        Box.body.restitution = 0.0;
+        Box.body.gravity = 500;
+        Box.body.static = false;
+        Box.body.fixedRotation = true;
+        Box.body.setCollisionGroup(BoxCollisionGroup);
+        Box.body.collides(isJumpCollisionGroup,function (){onGround = true;},this);
+        Box.body.collides([playerCollisionGroup]);
+
+    },
+    createKillObj: function(x, y, index, playerCollisionGroup, killCollisionGroup){
+        diamond = this.add.sprite(x, y, index);
+        this.physics.p2.enableBody(diamond,isDebug);
+        diamond.body.static = true;
+        diamond.body.fixedRotation = true;
+        diamond.body.setCollisionGroup(killCollisionGroup);
+        diamond.body.collides([playerCollisionGroup]);
+    },
     create: function() {
         //adds music
         this.music = this.add.audio('tutorialmusic');
@@ -23,6 +63,11 @@ Game.tutorial.prototype={
     	//changes bounds of the world and add a background for the world
     	this.world.setBounds(0,0,1400,this.world.height);
         this.add.tileSprite(0, 0,1400,this.world.height, 'fulldome');
+          //pause menu
+        this.btnPause = this.game.add.button(675,20,'pause',this.pauseGame,this);
+        this.pausePanel = new PausePanel(this.game);
+        this.game.add.existing(this.pausePanel);
+        this.playGame();
 
         //  We're going to be using physics, so enable the Arcade Physics system
         this.physics.startSystem(Phaser.Physics.P2JS);
@@ -31,10 +76,11 @@ Game.tutorial.prototype={
         this.physics.p2.restitution = 0.0;
 
         //COLLISION GROUPS -- VERY IMPORTANT (Helps keep track of which platforms the player can jump on...)
-        var playerCollisionGroup = this.physics.p2.createCollisionGroup();
-        var isJumpCollisionGroup = this.physics.p2.createCollisionGroup();
-        var winCollisionGroup = this.physics.p2.createCollisionGroup();
-        var killCollisionGroup = this.physics.p2.createCollisionGroup();
+        playerCollisionGroup = this.physics.p2.createCollisionGroup();
+        isJumpCollisionGroup = this.physics.p2.createCollisionGroup();
+        winCollisionGroup = this.physics.p2.createCollisionGroup();
+        killCollisionGroup = this.physics.p2.createCollisionGroup();
+        BoxCollisionGroup = this.physics.p2.createCollisionGroup();
         //  This part is vital if you want the objects with their own collision groups to still collide with the world bounds
         //  (which we do) - what this does is adjust the bounds to use its own collision group.
         this.physics.p2.updateBoundsCollisionGroup();
@@ -67,6 +113,7 @@ Game.tutorial.prototype={
         player.body.collides(isJumpCollisionGroup,function (){ifCanJump = true;},this);
         player.body.collides(winCollisionGroup, this.nextLevel, this);
         player.body.collides(killCollisionGroup, this.endGame, this);
+        player.body.collides(BoxCollisionGroup,function(){playerbox = true; ifCanJump = true;},this)
 
         //star that advances you to next level
         star = this.add.sprite(1330, 460, 'star');
@@ -75,54 +122,63 @@ Game.tutorial.prototype={
         star.body.collides([isJumpCollisionGroup, playerCollisionGroup]);
 
         //trap for tutorial
-        trap = this.add.sprite(200, 460, 'diamond');
-        this.physics.p2.enableBody(trap);
-        trap.body.setCollisionGroup(killCollisionGroup);
-        trap.body.collides([isJumpCollisionGroup, playerCollisionGroup]);
+        this.createKillObj(500, 490, 'diamond', playerCollisionGroup, killCollisionGroup);
+        this.createKillObj(560, 490, 'diamond', playerCollisionGroup, killCollisionGroup);
+        this.createKillObj(620, 490, 'diamond', playerCollisionGroup, killCollisionGroup);
+        this.createKillObj(680, 490, 'diamond', playerCollisionGroup, killCollisionGroup);
+        this.createKillObj(740, 490, 'diamond', playerCollisionGroup, killCollisionGroup);
+
+        //ladder to pass traps
+        ladder = this.add.sprite(620, 250, 'ladder');
+        this.createBox(100, 1700, 'diamond',playerCollisionGroup, isJumpCollisionGroup, BoxCollisionGroup);
 
         //sets camera to follow
         this.camera.follow(player);
-
-        jumpButton = this.input.keyboard.addKey(Phaser.Keyboard.UP);
 
         //  The score
         scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
 
         //  Our controls.
         cursors = this.input.keyboard.createCursorKeys();
-    	
-    	//pause menu
-    	pause_label = this.add.text(700, 20, 'Pause', { font: '32px Arial', fill: '#fff' });
-        pause_label.inputEnabled = true;
-        pause_label.events.onInputUp.add(function () {
-            // When the paus button is pressed, we pause the this
-            this.paused = true;
-    		console.log("this PAUSED IN CAMERA>> X: "+this.camera.x+" Y:"+this.camera.y);
-    		// click on diamond to unpause
-    		
-    		pause_label_continue = this.add.text(this.camera.x+400, this.camera.y+200, 'Continue',{ font: '32px Arial', fill: '#fff' });
-    		pause_label_continue.anchor.setTo(0.5,0.5);
-    		pause_label_continue.events.onInputUp.add(unpause, this);
+        jumpButton = this.input.keyboard.addKey(Phaser.Keyboard.UP);
+        pushButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    },
+    pauseGame: function(){
+        if(!paused){
+            paused = true;
+            this.pausePanel.show();
+            this.camera.unfollow();
+            this.physics.p2.gravity.y = 0;
+            player.body.velocity.x=0;
+            player.body.velocity.y=0;
+            
+            //add any object that is affected by gravity here.
+            mehSpeed.push(checkmark.body.velocity.x);
+            mehSpeed.push(checkmark.body.velocity.y);
+            
+            //Set the vbelocities to zero to make sure they dont move anymore.
+            checkmark.body.velocity.x = 0;
+            checkmark.body.velocity.y = 0;
+            
+            //fix the objects from rotating and make them static
+            checkmark.body.fixedRotation = true;
+        }
+    },
 
-    		pause_label_help = this.add.text(this.camera.x+400, this.camera.y+250, 'Help',{ font: '32px Arial', fill: '#fff' });
-        	pause_label_help.anchor.setTo(0.5,0.5);
-        	//pause_label_help.events.onInputUp.add(helpmenu, this);	//HELP FUNCTION LOADS THE HELP STUFF
-
-        	pause_label_exit = this.add.text(this.camera.x+400, this.camera.y+300, 'Exit',{ font: '32px Arial', fill: '#fff' });
-        	pause_label_exit.anchor.setTo(0.5,0.5);
-        	//pause_label_help.events.onInputUp.add(exitthis, this);	//Exit Function takes the this back to the menu
-    	});
-    		
-    	//this.input.onDown.add(unpause, self);
-    	
-    	function unpause(event) {
-    		if(this.paused) {
-    				pause_label_continue.destroy();
-    				pause_label_help.destroy();
-    				pause_label_exit.destroy();
-    				this.paused = false;
-    		}	
-    	};
+    playGame: function(){
+        if(paused){
+            paused = false;
+            this.pausePanel.hide();
+            this.camera.follow(player,this.camera.FOLLOW_PLATFORMER);
+            this.physics.p2.gravity.y = 500;
+            
+            //Push out velocties affected by gravity for objects here.
+            checkmark.body.velocity.y = mehSpeed.pop();
+            checkmark.body.velocity.x = mehSpeed.pop();
+            
+            //allow for totations and disable static.
+            checkmark.body.fixedRotation = false;
+        }
     },
 
     update: function() {
@@ -131,56 +187,133 @@ Game.tutorial.prototype={
         //  To move the UI along with the camera 
         scoreText.x = this.camera.x+16;
         scoreText.y = this.camera.y+16;
-        pause_label.x = this.camera.x+700;
-        pause_label.y = this.camera.y+20;
+        this.btnPause.x = this.camera.x+675;
+        this.btnPause.y = this.camera.y+20;
+        this.pausePanel.x = this.camera.x+655;
+        if(pushButton.isDown && ((player.body.x >= 620 && player.body.x <= 620+20 && player.body.y >= 250 && player.body.y <= 250+150))){
+            console.log("on ladder");
+            player.body.data.gravityScale=0.05;
+            onLadder=true;
+        }
+        else{
+            player.body.data.gravityScale=1;
+            onLadder=false;
+        }
+
+        if(!paused){
+                this.pausePanel.y = this.camera.y-100;
+                this.pausePanel.update();
+        }
 
         //  Reset the players velocity (movement)
         player.body.velocity.x = 0;
-
-        if (cursors.left.isDown)
-        {
-            player.body.moveLeft(200);
-
-            if (facing != 'left')
-            {
-                player.animations.play('left');
-                facing = 'left';
-            }
+        if(paused || inWater){
+            player.body.velocity.y = 0;
         }
-        else if (cursors.right.isDown)
-        {
-            player.body.moveRight(200);
-
-            if (facing != 'right')
+      //Control Player Movement;
+        if (!paused && !inWater && !onLadder){
+            if (cursors.left.isDown)
             {
-                player.animations.play('right');
-                facing = 'right';
-            }
-        }
-        else
-        {
-            player.body.velocity.x = 0;
+                player.body.moveLeft(200+godmode);
 
-            if (facing != 'idle')
-            {
-                player.animations.stop();
-
-                if (facing == 'left')
+                if (facing != 'left')
                 {
-                    player.frame = 0;
+                    player.animations.play('left');
+                    facing = 'left';
                 }
-                else
-                {
-                    player.frame = 5;
-                }
+            }
+            else if (cursors.right.isDown)
+            {
+                player.body.moveRight(200+godmode);
 
-                facing = 'idle';
+                if (facing != 'right')
+                {
+                    player.animations.play('right');
+                    facing = 'right';
+                }
+            }
+            else if(ifCanJump)
+            {
+                player.body.velocity.x = 0;
+
+                if (facing != 'idle')
+                {
+                    player.animations.stop();
+
+                    if (facing == 'left')
+                    {
+                        player.frame = 0;
+                    }
+                    else
+                    {
+                        player.frame = 5;
+                    }
+
+                    facing = 'idle';
+                }
+            }
+
+            if (jumpButton.isDown && ifCanJump){
+                player.body.moveUp(300+godmode);
+                ifCanJump = false;
+            }
+            // moving a Box-----------------------------
+            if ((pushButton.isDown && playerbox) || (pushButton.isDown && playerbox)) {
+                onGround = false;
+                if (checkCreated < 1){
+                    onGround = false;
+                    Box.body.destroy();
+                    Box.kill();
+                    this.createBox(boxX, boxY, 'diamond',playerCollisionGroup, isJumpCollisionGroup, BoxCollisionGroup);
+                    checkCreated++;
+                }
+            }else if (pushButton.isUp && onGround){
+            
+                Box.body.static = true;
+                boxX = Box.body.x;
+                boxY = Box.body.y;
+                checkCreated =0;
+                playerbox =false;
+                
+            }
+
+        }
+
+        if (!paused && inWater && !onLadder){
+            if (cursors.left.isDown)
+            {
+                player.body.moveLeft(200+godmode);
+                if (facing != 'left')
+                {
+                    player.animations.play('left');
+                    facing = 'left';
+                }
+            }
+            else if (cursors.right.isDown)
+            {
+                player.body.moveRight(200+godmode);
+                if (facing != 'right')
+                {
+                    player.animations.play('right');
+                    facing = 'right';
+                }
+            }
+            if (cursors.up.isDown&&!inWater)
+            {
+                player.body.moveUp(200+godmode);
+            }
+            else if (cursors.down.isDown)
+            {
+                player.body.moveDown(200+godmode);
             }
         }
-        
-        if (jumpButton.isDown && ifCanJump){
-            player.body.moveUp(300);
-            ifCanJump = false;
+        if(!paused && !inWater && onLadder){
+            if(cursors.up.isDown){
+                player.body.moveUp(40);
+            }
+            else if(cursors.down.isDown){
+                player.body.moveDown(40);
+            }
         }
     },
 
@@ -193,4 +326,72 @@ Game.tutorial.prototype={
         this.music.stop();
         this.state.start('main');
     }
+};
+
+var PausePanel = function(game, parent){
+    //Super call to Phaser.group
+    Phaser.Group.call(this, game, parent);
+
+    this.btnPlay = this.game.add.button(20,20,'continue',function(){
+        this.game.state.getCurrentState().playGame()
+    },this);
+    this.add(this.btnPlay);
+
+    //place it out of bounds
+    this.x = 655;
+    this.y = -100;
+    
+    btnRestart = this.game.add.button(350,-225,'restart',function(){
+        this.game.state.restart(true,true);
+    },this);
+
+    btnHelpScreen = this.game.add.button(150,-500,'helpscn',function(){
+        this.game.add.tween(btnHelpScreen).to({y:this.game.camera.y-600}, 200, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnRestart).to({y:this.game.camera.y+175}, 500, Phaser.Easing.Bounce.Out, true);
+        this.game.add.tween(btnHelp).to({y:this.game.camera.y+250}, 500, Phaser.Easing.Bounce.Out, true);
+        this.game.add.tween(btnQuit).to({y:this.game.camera.y+325}, 500, Phaser.Easing.Bounce.Out, true);
+    },this);
+
+    btnHelp = this.game.add.button(350,-150,'help',function(){
+        this.game.add.tween(btnHelpScreen).to({y:this.game.camera.y+50}, 500, Phaser.Easing.Bounce.Out, true);
+        this.game.add.tween(btnRestart).to({y:this.game.camera.y-225}, 200, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnHelp).to({y:this.game.camera.y-150}, 200, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnQuit).to({y:this.game.camera.y-75}, 200, Phaser.Easing.Linear.NONE, true);
+    },this);
+
+    btnQuit = this.game.add.button(350,-75,'quit',function(){
+        this.game.state.start('mainmenu');
+    },this);
+};
+
+PausePanel.prototype = Object.create(Phaser.Group.prototype);
+PausePanel.constructor = PausePanel;
+
+PausePanel.prototype.show = function(){
+    this.game.add.tween(this).to({y:this.game.camera.y+0}, 500, Phaser.Easing.Bounce.Out, true);
+    this.game.add.tween(btnRestart).to({y:this.game.camera.y+175}, 500, Phaser.Easing.Bounce.Out, true);
+    this.game.add.tween(btnHelp).to({y:this.game.camera.y+250}, 500, Phaser.Easing.Bounce.Out, true);
+    this.game.add.tween(btnQuit).to({y:this.game.camera.y+325}, 500, Phaser.Easing.Bounce.Out, true);
+};
+PausePanel.prototype.update = function(){
+    if(!paused){
+        this.game.add.tween(btnRestart).to({x:this.game.camera.x+350}, 1, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnHelp).to({x:this.game.camera.x+350}, 1, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnQuit).to({x:this.game.camera.x+350}, 1, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnHelpScreen).to({x:this.game.camera.x+150}, 1, Phaser.Easing.Linear.NONE, true);
+        //for Y-axis
+        this.game.add.tween(btnRestart).to({y:this.game.camera.y-225}, 1, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnHelp).to({y:this.game.camera.y-150}, 1, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnQuit).to({y:this.game.camera.y-75}, 1, Phaser.Easing.Linear.NONE, true);
+        this.game.add.tween(btnHelpScreen).to({y:this.game.camera.y-600}, 1, Phaser.Easing.Linear.NONE, true);
+    }
 }
+
+PausePanel.prototype.hide = function(){
+    this.game.add.tween(btnHelpScreen).to({y:this.game.camera.y-500}, 200, Phaser.Easing.Linear.NONE, true);
+    this.game.add.tween(this).to({y:this.game.camera.y-100}, 200, Phaser.Easing.Linear.NONE, true);
+    this.game.add.tween(btnRestart).to({y:this.game.camera.y-225}, 200, Phaser.Easing.Linear.NONE, true);
+    this.game.add.tween(btnHelp).to({y:this.game.camera.y-150}, 200, Phaser.Easing.Linear.NONE, true);
+    this.game.add.tween(btnQuit).to({y:this.game.camera.y-75}, 200, Phaser.Easing.Linear.NONE, true);
+};
+
